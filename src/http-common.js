6bde1440e3e5f9c8892caa11f3d7d9632d03bf5d
kiaathearts <goodenekia@yahoo.com>
2020-10-05 07:14:59 -0400

    Update http-common address to point to our express server

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 export default axios.create({
   // baseURL: "http://localhost:8080",
-  baseURL: "ec2-54-166-36-127.compute-1.amazonaws.com",
+  // baseURL: "ec2-54-166-36-127.compute-1.amazonaws.com",
+  baseURL: "ec2-18-234-216-12.compute-1.amazonaws.com",
   headers: {
     "Content-type": "application/json"
   }
