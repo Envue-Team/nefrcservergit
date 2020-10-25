cf272929b7e1b0174b34548c02cb72c211a9b6a2
Envue-Team <envue.team@gmail.com>
2020-10-24 21:10:58 -0400

    Login page
    proper login page working

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="home">
+    <div id="nav">
+      <router-link to="/home">Home</router-link> |
+      <router-link to="/about">About</router-link> | 
+      <router-link to="/volunteer/1">Volunteer 1</router-link> |
+      <router-link to="/volunteers-list">Volunteers</router-link> | 
+      <router-link to="/add-shelter">Add Shelter</router-link> |
+      <router-link to="/shelters-list">Shelters</router-link>
+    </div>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
