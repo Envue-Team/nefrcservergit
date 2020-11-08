e9fe829514f444abb1dcde4cc99186d6fe47f906
team-envue <>
2020-11-08 11:33:18 -0500

    revert commit
    reverting commit

--- src/router.js
+++ src/router.js
@@ -1,11 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+<<<<<<< HEAD
 import Organization from './views/Organizations.vue'
 import Partner from './views/Partner.vue'
 import Relationship from './views/Relationship.vue'
 import Contacts from './views/Contacts.vue'
 
+=======
+import Volunteer from './views/Volunteer.vue'
+import Volunteers from './views/ListVolunteers.vue'
+import AddVolunteer from './views/AddVolunteer.vue'
+import AddShelter from './views/AddShelter.vue'
+import Shelters from './views/ListShelters.vue'
+>>>>>>> parent of 1bb00a0e... Merge pull request #1 from Envue-Team/UserAccounts
 
 Vue.use(Router)
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,12 @@
 <template>
+<<<<<<< HEAD
   <hello-world />
+=======
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+>>>>>>> parent of 1bb00a0e... Merge pull request #1 from Envue-Team/UserAccounts
 </template>
 
 <script>
