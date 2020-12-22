a294401bfd1df2785fb0715066323f310c022062
Jason Bolling <jason.bolling11@gmail.com>
2020-12-22 15:44:26 -0500

    added files kia sent to me(relationshipmanagerdialog.vue, organization.vue, and notedialog.vue

---
+++ src/views/RelationshipManagerDialog.vue
@@ -0,0 +1,171 @@
+<template>
+  <v-container>
+    <!---------------------------------Relationship Manager Dialog------------------------------->
+    <v-dialog
+        max-width="600px"
+        v-model="show"
+    >
+      <v-card>
+        <v-form>
+          <v-card-title>
+            <span class="headline">{{rm_title}}</span>
+          </v-card-title>
+          <v-card-text>
+            <v-container>
+              <v-row>
+                <v-col
+                    cols="12"
+                    sm="12"
+                    md="12"
+                >
+                  <p class="text-h4" v-show="rm_dlg_action=='Edit'">
+                    {{relationship_manager.first_name}} {{relationship_manager.last_name}}
+                  </p>
+                  <v-autocomplete
+                      v-show="rm_dlg_action=='Create'"
+                      label="Relationship Manager"
+                      :items="all_relationship_managers"
+                      item-text="name"
+                      item-value="value"
+                      return-object
+                      @change="updateSelectedManager"
+                  >
+                  </v-autocomplete>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+                color="blue darken-1"
+                text
+                @click.stop="show=false"
+            >
+              Close
+            </v-btn>
+            <v-btn v-show="rm_dlg_action=='Edit'"
+                color="red darken-1"
+                text
+                v-on:click="openDialog('Delete')"
+            >
+              Remove Manager
+            </v-btn>
+            <v-btn
+                v-show="rm_dlg_action=='Create'"
+                color="blue darken-1"
+                text
+                v-on:click="openDialog(rm_dlg_action)"
+            >
+              Save
+            </v-btn>
+          </v-card-actions>
+        </v-form>
+      </v-card>
+    </v-dialog>
+
+    <!---------------------------------Delete Relationship Manager Dialog------------------------------>
+    <v-dialog
+        max-width="300"
+        v-model="delete_relationship_manager_dialog"
+    >
+      <v-card>
+        <v-card-text>
+          Are you sure you want to remove this relationship manager?
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-btn
+              @click="deleteRelationshipManager"
+              text
+              color="red text-darken-1">
+            Yes
+          </v-btn>
+          <v-btn
+              @click="delete_relationship_manager_dialog=false"
+              text
+              color="blue text-darken-1"
+          >
+            No
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <!---------------------------------//Delete Relationship Manager Dialog------------------------------>
+  </v-container>
+</template>
+
+<script>
+
+import RelationshipManagerDataService from "@/services/RelationshipManagerDataService";
+
+export default {
+  props: {
+    value: Boolean,
+    rm_title: String,
+    all_relationship_managers: Array,
+    rm_dlg_action: String,
+    current_relationship_manager_id: String,
+    relationship_manager: Object
+  },
+  data(){
+    return {
+      delete_relationship_manager_dialog: false,
+      save_rm_dialog: false,
+      updated_relationship_manager: '',
+      organization_relationship_managers: []
+    }
+  },
+  methods:{
+    updateSelectedManager(obj){
+      this.updated_relationship_manager = {
+        value: obj.value
+      };
+    },
+    openDialog(dlg){
+      switch(dlg){
+        case 'Create':
+          this.addRelationshipManager();
+          break;
+        case 'Delete':
+          this.delete_relationship_manager_dialog = true;
+          break;
+        case 'Edit':
+          this.updateRelationshipManager();
+          break;
+      }
+    },
+    addRelationshipManager(){
+      let data = {
+        personId: this.updated_relationship_manager.value
+      }
+      this.$parent.addRelationshipManager(data);
+
+    },
+    updateRelationshipManager(){
+      this.update_mgr_dlg = false;
+      let data = {
+        personId: this.updated_relationship_manager.value
+      };
+      this.$parent.updateRelationshipManager(data, this.current_relationship_manager_id);
+    },
+    deleteRelationshipManager(){
+      this.update_mgr_dlg = false;
+      this.delete_relationship_manager_dialog = false;
+      this.$emit('input', false);
+      this.$parent.deleteRelationshipManager(this.current_relationship_manager_id);
+    },
+  },
+  computed: {
+    show: {
+      get () {
+        return this.value
+      },
+      set (value) {
+        this.$emit('input', value)
+      }
+    },
+
+  },
+};
+</script>
