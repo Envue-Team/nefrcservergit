0df1887ab8310352a179fff2fe10a472113c3f1f
kiaathearts <goodenekia@yahoo.com>
2021-02-08 06:26:30 -0500

    Update theme to dashboard console - Post update live site

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: "http://localhost:8080",
-  // baseURL: "http://ec2-3-84-201-28.compute-1.amazonaws.com:8080",
+  // baseURL: "http://ec2-34-201-103-158.compute-1.amazonaws.com:8080",
   headers: {
     "Content-type": "application/json",
     
