74809bb23b3ac05dd164322059575d1e0ebb2ba0
Envue-Team <68168995+Envue-Team@users.noreply.github.com>
2021-03-22 08:42:40 -0400

    Update mailer.controller.js

--- src/controllers/mailer.controller.js
+++ src/controllers/mailer.controller.js
@@ -1,20 +1,9 @@
-// Create and Save a new email number
-exports.sendMail = (req, res) => {
-    let transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 587,
-        secure: false, // true for 465, false for other ports
-        auth: {
-            user: "team.envue@gmail.com", // generated ethereal user
-            pass: "password", // generated ethereal password
-        },
-    });
-    let info = transporter.sendMail({
-        from: '"Fred Foo 👻" <foo@example.com>', // sender address
-        subject: "Hello ✔", // Subject line
-        text: "Hello world?", // plain text body
-        html: "<b>Hello world?</b>", // html body
-    });
-    console.log("Message sent: %s", info.messageId);
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+module.exports = volapp => {
+    const mailer = require("../controllers/mailer.controller.js");
+    var router = require("express").Router();
+
+    // Send
+    router.post("/", mailer.sendMail);
+
+    volapp.use('/emailer', router);
 }
