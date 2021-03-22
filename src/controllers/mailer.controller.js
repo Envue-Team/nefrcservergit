ac639ef18ba892f696201e707eae43a35a98630d
kiaathearts <goodenekia@yahoo.com>
2021-03-22 08:57:45 -0400

    Update emailer to latest

--- src/controllers/mailer.controller.js
+++ src/controllers/mailer.controller.js
@@ -1,20 +1,23 @@
+const nodemailer = require('nodemailer');
+
 // Create and Save a new email number
 exports.sendMail = (req, res) => {
+    console.log("called");
     let transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
         port: 587,
         secure: false, // true for 465, false for other ports
         auth: {
             user: "team.envue@gmail.com", // generated ethereal user
-            pass: "password", // generated ethereal password
+            pass: "somepass", // generated ethereal password
         },
     });
+
     let info = transporter.sendMail({
-        from: '"Fred Foo 👻" <foo@example.com>', // sender address
-        subject: "Hello ✔", // Subject line
-        text: "Hello world?", // plain text body
-        html: "<b>Hello world?</b>", // html body
+        from: '"Envue 👻" <team.envue@gmail.com>', // sender address
+        to: req.body.sendTo,
+        subject: req.body.subject, // Subject line
+        text: req.body.text, // plain text body
+        html: req.body.html, // html body
     });
-    console.log("Message sent: %s", info.messageId);
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-}
+};
